10daf7e47ff869b48445ed742b1f924555bca904
Ronell Salunke <ronell1292k@gmail.com>
2024-02-14 01:05:50 +0530

    feat: add a basic frontend for uploading images

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 import helper
 
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-    return {"ok"}
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return """
+    <html>
+        <head>
+            <title>Image Upload</title>
+        </head>
+        <body>
+            <h1>Upload an Image</h1>
+            <form action="/predict/" method="post" enctype="multipart/form-data">
+                <input type="file" name="file">
+                <input type="submit" value="Upload">
+            </form>
+        </body>
+    </html>
+    """
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
